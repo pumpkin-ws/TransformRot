1789a23e671c143946a7d3610758bb652b4143a7
JHL-self-program-robot_pumpkin_wang <pumpkin_wang@foxmail.com>
2022-05-15 22:32:43 +0800

    add regex test

---
+++ tests/regex_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <regex>
+
+#define LOG_RED(...) printf("This is a good place to start our journey.\n")
+
+int main(int argc, char** argv) {
+    LOG_RED("SOME ARGS");
+}
